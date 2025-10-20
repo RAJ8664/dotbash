730fb828c6ea03b561a9a801da4e603f577c122e
rkroy <rajr86642@gmail.com>
2025-10-20 15:27:28 +0530

    add kitty configs

---
+++ scripts/input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Made for the purpose of competitive programming.
+# File to write
+FILE="input.txt"
+
+# Get clipboard content using wl-paste and save to file
+if wl-paste >"$FILE"; then
+    echo "Clipboard content written to $FILE"
+else
+    echo "Error: Could not retrieve clipboard content."
+fi
